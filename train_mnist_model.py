b713277527e62f2892641f704c0a80ed5a2078a7
Muhammad Hassan Raza <154966489+MuhammadHassanRaza63@users.noreply.github.com>
2025-06-24 13:41:35 +0500

    Add files via upload

---
+++ train_mnist_model.py
@@ -0,0 +1,34 @@
+# train_mnist_model.py
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.utils import to_categorical
+
+# Load data
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Reshape and normalize
+x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255
+x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# Model
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
+    MaxPooling2D(2,2),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=64, validation_data=(x_test, y_test))
+
+# Save
+model.save('mnist_model.h5')
